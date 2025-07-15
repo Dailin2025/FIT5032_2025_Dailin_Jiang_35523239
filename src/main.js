82b59d79abc7bb3030e9a99246222d039a8e89d3
Dailin2025 <djia0020@student.monash.edu>
2025-07-15 11:27:28 +0800

    Realize Assessed Lab4

--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
 // import JSON from './components/JSON.vue'
-createApp(App).mount('#app')
+
+const app = createApp(App);
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura
+  }
+});
+
+app.mount("#app");
 
